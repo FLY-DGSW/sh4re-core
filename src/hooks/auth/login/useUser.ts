a5ba8a4381131180c0a5c16502d9a5d426f4dc6e
chaeyn <chaulfe@gmail.com>
2025-08-02 14:16:37 +0900

    fix: accessToken 있을 때만 useUser query 요청 보내기

--- src/hooks/auth/login/useUser.ts
+++ src/hooks/auth/login/useUser.ts
@@ -1,12 +1,18 @@
 import sh4reCustomAxios from "@/api/sh4reCustomAxios";
+import { ACCESS_TOKEN_KEY } from "@/constants/token.constants";
+import token from "@/libs/token/token";
 import { useQuery } from "@tanstack/react-query";
 
-export const useUser = () =>
-  useQuery({
+export const useUser = () => {
+  const accessToken = token.getToken(ACCESS_TOKEN_KEY);
+
+  return useQuery({
     queryKey: ["user"],
     queryFn: async () => {
       const { data } = await sh4reCustomAxios.get("/user/me");
       return data.data.me;
     },
     staleTime: 5 * 60 * 1000,
+    enabled: !!accessToken,
   });
+};
